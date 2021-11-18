eebbba549f2f68353918c96b558bc1411e8bbb0b
fusho-takahashi <zenryaku@fusho-takahashi.me>
2021-11-18 15:23:45 +0900

    1 file modified

--- src/app/counter-store.service.ts
+++ src/app/counter-store.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { BehaviorSubject, Observable, queueScheduler } from 'rxjs';
+import { distinctUntilChanged, map } from 'rxjs/operators';
 
 interface State {
   count: number;
@@ -14,37 +14,45 @@ export class CounterStoreService {
    * 全部返すとき
    */
   get state$(): Observable<State> {
-    return this._counterState$.asObservable();
+    return this.select<State>((state) => state);
   }
 
   /**
    * 特定のプロパティだけほしいとき
    */
   get count$(): Observable<number> {
-    return this._counterState$.asObservable().pipe(map((state) => state.count));
+    return this.select<number>((state) => state.count);
   }
 
   /**
    * 同期的に値がほしいとき
    */
   get countValu(): number {
-    return this._counterState$.getValue().count;
+    return this._counterState$.value.count;
+  }
+
+  select<T>(selector: (state: State) => T) {
+    return this._counterState$.pipe(map(selector), distinctUntilChanged());
   }
 
   incrementCount(): void {
-    const currentValue = this._counterState$.getValue();
-    this._counterState$.next({
-      ...currentValue,
-      count: currentValue.count + 1,
+    const currentValue = this._counterState$.value;
+    queueScheduler.schedule(() => {
+      this._counterState$.next({
+        ...currentValue,
+        count: currentValue.count + 1,
+      });
     });
   }
 
   decrementCount(): void {
-    const currentValue = this._counterState$.getValue();
+    const currentValue = this._counterState$.value;
     if (currentValue.count < 1) return;
-    this._counterState$.next({
-      ...currentValue,
-      count: currentValue.count - 1,
+    queueScheduler.schedule(() => {
+      this._counterState$.next({
+        ...currentValue,
+        count: currentValue.count - 1,
+      });
     });
   }
 }
